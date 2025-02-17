40b510d9f109bceff7b2be33b3eb6ef061e5743f
Adrian2k25-cpu <adroman23-0290@cca.edu.ph>
2025-02-17 18:43:55 +0800

    form

--- index.php
+++ index.php
@@ -2,19 +2,52 @@
 	<head>
 		<title>Javascript Form</title>
 	</head>
-	<body>
-	<h1> Student Record</h1>
+
+	<body style="background-color: black; color: #FFF;">
+		<h1>Siomai Rice</h1>
 	<table id="student_table">
 	</table>
-	
+
 		<script>
-		
-		const studentTable = document
-			.getElementById("student_table")
-		console.log(studentTable);
-		
+	const studentTable= document
+	.getElementById("student_table");
+
+	console.log(studentTable);
+	const row1 = studentTable.insertRow();
+
+	const row1col1 = row1.insertCell();
+	const row1col2 = row1.insertCell();
+
+	row1col1.innerHTML = "LAST NAME";
+	row1col2.innerHTML = "FIRST NAME";
+
+	const row2 = studentTable.insertRow();
+	const row2col1 = row2.insertCell();
+	const row2col2 = row2.insertCell();
+
+	const inputLastName = document.createElement("Input");
+	inputLastName.type="text";
+	inputLastName.placeholder="Inster Last Name";
+
+	row2col1.append (inputLastName);
+
+	const inputFirstName = document.createElement("input");
+	inputFirstName.type="text";
+	inputFirstName.placeholder="Insert First Name";
+
+	row2col2.append (inputFirstName);
+
+	const row3 = studentTable.insertRow();
+	const row3col1 = row3.insertCell();
+	row3col1.colSpan="2";
+	
+	
+	const inputSubmit = document.createElement("input");
+	inputSubmit.type="submit";
+	inputSubmit.value="Create Record";
+	
+	row3col1.append(inputSubmit);
+
 		</script>
 	</body>
 </html>
-	
-		
